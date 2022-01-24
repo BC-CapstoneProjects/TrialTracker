81f4fa3c7eaa3ddb9eba9b8cd5057a0341d48ff3
Kylie Nicholson <kylienicholson99@gmail.com>
2022-01-24 06:28:53 -0800

    Bug fix
    Welcome screen now runs

--- app/src/main/java/com/kinisi/trailtracker/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/kinisi/trailtracker/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Hi user! \nLet's get moving"
     }
     val text: LiveData<String> = _text
 }
