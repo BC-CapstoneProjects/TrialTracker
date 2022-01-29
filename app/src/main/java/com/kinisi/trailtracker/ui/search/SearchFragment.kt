eb917ca359d979b674a731b65b5ed99298b517a7
bijanazodi <bijan.azodi@bellevuecollege.edu>
2022-01-29 10:30:59 -0800

    "added map fragment"

--- app/src/main/java/com/kinisi/trailtracker/ui/search/SearchFragment.kt
+++ app/src/main/java/com/kinisi/trailtracker/ui/search/SearchFragment.kt
@@ -1,45 +1,69 @@
 package com.kinisi.trailtracker.ui.search
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.kinisi.trailtracker.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.kinisi.trailtracker.databinding.FragmentSearchBinding
+import com.google.android.gms.maps.GoogleMap
+import android.location.Geocoder
+
+
+import android.R
+import android.location.Address
+import android.util.Log
+
+import android.widget.EditText
+import com.kinisi.trailtracker.MainActivity
+import java.io.IOException
+import java.util.*
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment
+
+
+
+
 
 class SearchFragment : Fragment() {
+    private val callback = OnMapReadyCallback { googleMap ->
 
-    private lateinit var searchViewModel: SearchViewModel
-    private var _binding: FragmentSearchBinding? = null
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        searchViewModel =
-            ViewModelProvider(this).get(SearchViewModel::class.java)
-
-        _binding = FragmentSearchBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textSearch
-        searchViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(com.kinisi.trailtracker.R.layout.fragment_maps, container, false)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(com.kinisi.trailtracker.R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
 }
