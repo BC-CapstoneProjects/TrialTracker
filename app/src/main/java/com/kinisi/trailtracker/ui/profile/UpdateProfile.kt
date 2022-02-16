deb4447b9daadc72bd30b730512434354f8c946b
Kylie Nicholson <kylienicholson99@gmail.com>
2022-02-15 20:10:53 -0800

    Connect to Firebase
    Users are able to update their settings and store their info in firebase

--- app/src/main/java/com/kinisi/trailtracker/ui/profile/UpdateProfile.kt
+++ app/src/main/java/com/kinisi/trailtracker/ui/profile/UpdateProfile.kt
@@ -4,49 +4,86 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.Toast
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.firestore.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.kinisi.trailtracker.R
 import kotlinx.android.synthetic.main.activity_update_profile.*
-
+import java.util.*
+import android.widget.RadioGroup.*
 class UpdateProfile : AppCompatActivity() {
-   // FirebaseFirestore db = FirebaseFirestore.getInstance()
-
-
+    lateinit var auth: FirebaseAuth
+    //lateinit var db: Fire
+    //Firebase db = FirebaseFirestore.getInstance()
+    val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.kinisi.trailtracker.R.layout.activity_update_profile)
+        auth=FirebaseAuth.getInstance()
 
-        btnUpdateImage.setOnClickListener {
-            // update the profile image here
-            val picUpdate = UserProfileChangeRequest.Builder()
-                .setPhotoUri(Uri.parse("new.photo.url.here"))
-                .build()
+        //Set maximum date to current date
+        dob.setMaxDate(Date().time)
+       // val name = db.collection("users/"+auth.currentUser!!.uid+"/name");
 
-            FirebaseAuth.getInstance().currentUser?.updateProfile(picUpdate)
-        }
 
-        btnUpdateName.setOnClickListener {
-            // update the first name here
-            val nameUpdate = UserProfileChangeRequest.Builder()
-                .setDisplayName(Name.text.toString())
-                .build()
 
-            FirebaseAuth.getInstance().currentUser?.updateProfile(nameUpdate)
-        }
 
-        btnUpdateHeight.setOnClickListener {
-            // update the name here
-            val nameUpdate = UserProfileChangeRequest.Builder()
-                .setDisplayName(height.text.toString())
-                .build()
+        btnUpdateProfile.setOnClickListener {
+                    //Update image
 
-            FirebaseAuth.getInstance().currentUser?.updateProfile(nameUpdate)
-        }
+                    //Collect info
+                    val name = Name.text.toString()
+                    val DOB = Timestamp(Date(dob.year - 1900, dob.month, dob.dayOfMonth))
+                    val height = height.text.toString()
+                    val weight = weight.text.toString()
+                    val sex = sex_RadioGroup.findViewById<RadioButton>(sex_RadioGroup.checkedRadioButtonId).text.toString()
+
+                    //Set info to user
+                    val user = hashMapOf(
+                        "name" to name,
+                        "height" to height,
+                        "weight" to weight,
+                        "dob" to DOB,
+                        "sex" to sex
+                    )
+                    auth.currentUser?.let { it1 ->
+                        db.collection("users").document(auth.currentUser!!.uid)
+                            .set(user)
+                            .addOnSuccessListener {
+                                Log.d(
+                                    "docSnippets",
+                                    "DocumentSnapshot successfully written!"
+                                )
+                            }
+                            .addOnFailureListener { e ->
+                                Log.w(
+                                    "docSnippets",
+                                    "Error writing document",
+                                    e
+                                )
+                            }
+                    }
+
+                }
+
+//        btnUpdateImage.setOnClickListener {
+//            // update the profile image here
+//            val picUpdate = UserProfileChangeRequest.Builder()
+//                .setPhotoUri(Uri.parse("new.photo.url.here"))
+//                .build()
+//            FirebaseAuth.getInstance().currentUser?.updateProfile(picUpdate)
+//        }
+
 
 
     }
