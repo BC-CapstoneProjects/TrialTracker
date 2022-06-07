0f77dbe13f2de1ab2107278d91ad5eb0aeb64879
Mac DeCourcy <macdecourcy@Macs-MacBook-Pro.local>
2022-06-07 11:22:06 -0700

    barebones search up

--- app/src/main/java/com/kinisi/trailtracker/models/DataModel.kt
+++ app/src/main/java/com/kinisi/trailtracker/models/DataModel.kt
@@ -3,3 +3,9 @@ package com.kinisi.trailtracker.models
 import com.google.gson.annotations.SerializedName
 
 data class DataModel(val id: String, val title: String, val type: String, val dist: String)
+
+data class SearchModel(val id: String, val title: String, val type: String, val xCoord: String, val yCoord: String)
+
+data class ResultsModel(val res: String)
+
+
--- app/src/main/java/com/kinisi/trailtracker/ui/newactivity/NewActivityFragment.kt
+++ app/src/main/java/com/kinisi/trailtracker/ui/newactivity/NewActivityFragment.kt
@@ -1,29 +1,31 @@
 package com.kinisi.trailtracker.ui.newactivity
 
+
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.fragment_favorites.*
-
+import com.kinisi.trailtracker.databinding.FragmentDashboardBinding
+import com.kinisi.trailtracker.models.DataModel
+import com.kinisi.trailtracker.models.SearchModel
+import kotlinx.android.synthetic.main.fragment_dashboard.*
+import kotlinx.android.synthetic.main.fragment_favorites.recycler_view
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONObject
+import org.json.JSONTokener
+import java.io.OutputStreamWriter
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
-
-
-import android.util.Log
-
-import com.kinisi.trailtracker.models.DataModel
-import org.json.JSONArray
-import org.json.JSONTokener
-import kotlin.collections.ArrayList
-import com.kinisi.trailtracker.databinding.FragmentDashboardBinding
 
 class NewActivityFragment : Fragment() {
 
@@ -36,13 +38,25 @@ class NewActivityFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         binding = FragmentDashboardBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
-        parseJSON()
-        var tmpArr: ArrayList<DataModel> = ArrayList()
+        // parseJSON()
+        searchButton.setOnClickListener {
+            execQuery(editText.getText().toString())
+        }
+        editText.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                // Toast.makeText(requireActivity(), "Great success", Toast.LENGTH_SHORT).show()
+                execQuery(editText.getText().toString())
+                return@OnKeyListener true
+            }
+            false
+        })
+        var tmpArr: ArrayList<SearchModel> = ArrayList()
         super.onViewCreated(itemView, savedInstanceState)
         recycler_view.apply {
             // set a LinearLayoutManager to handle Android
@@ -55,7 +69,129 @@ class NewActivityFragment : Fragment() {
 
     }
 
-    fun parseJSON(): java.util.ArrayList<DataModel> {
+    @SuppressLint("NotifyDataSetChanged")
+    fun execQuery(userQuery: String): java.util.ArrayList<SearchModel>  {
+        val values = mapOf("query" to userQuery)
+        val headers = mapOf("Content" to "application/json", "Authorization" to "Bearer search-ngvhsxdu1hjd3rzb433px13e")
+
+        val url = URL("https://kinisi-search-6f45a3.ent.us-central1.gcp.cloud.es.io/api/as/v1/engines/kinisi-search/search")
+        var ret = String()
+        var tmpArr: java.util.ArrayList<SearchModel> = java.util.ArrayList()
+        /* with(url.openConnection() as HttpURLConnection) {
+            requestMethod = "POST"
+            setRequestProperty("Content", "application/json")
+            setRequestProperty("Accept", "application/json")
+            setRequestProperty("Authorization", "Bearer search-ngvhsxdu1hjd3rzb433px13e")
+
+            val reqString = "{\"query\": \"$userQuery\"}"
+
+            val wr = OutputStreamWriter(getOutputStream());
+            wr.write(reqString);
+            wr.flush();
+
+            println("URL : $url")
+            println("Response Code : $responseCode")
+
+            BufferedReader(InputStreamReader(inputStream)).use {
+                val response = StringBuffer()
+
+                var inputLine = it.readLine()
+                while (inputLine != null) {
+                    response.append(inputLine)
+                    inputLine = it.readLine()
+                }
+                ret = response
+                println("Response : $response")
+            }
+
+        }
+        return ret*/
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val url = URL("https://kinisi-search-6f45a3.ent.us-central1.gcp.cloud.es.io/api/as/v1/engines/kinisi-search/search")
+            // URL("https://eflask-kinisi.herokuapp.com/api/closest&n=5&45.9604792,-123.6870344")
+            val httpsURLConnection = url.openConnection() as HttpsURLConnection
+            httpsURLConnection.setRequestProperty(
+                "Accept", "application/json"
+            )
+            httpsURLConnection.setRequestProperty(
+                "Content", "application/json"
+            )
+            httpsURLConnection.setRequestProperty(
+                "Authorization", "Bearer search-ngvhsxdu1hjd3rzb433px13e"
+            )
+            // The format of response we want to get from the server
+            httpsURLConnection.requestMethod = "POST"
+            httpsURLConnection.doInput = true
+            httpsURLConnection.doOutput = true
+            val reqString = "{\"query\": \"$userQuery\"}"
+            val wr = OutputStreamWriter(httpsURLConnection.getOutputStream())
+            wr.write(reqString);
+            wr.flush();
+
+            println("URL : $url")
+            // println("Response Code : $responseCode")
+
+            // Check if the connection is successful
+            val responseCode = httpsURLConnection.responseCode
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                val response = httpsURLConnection.inputStream.bufferedReader()
+                    .use { it.readText() }  // defaults to UTF-8
+                withContext(Dispatchers.Main) {
+                    // println(response.toString())
+                    // ret = response
+
+                    val jsonObject = JSONTokener(response).nextValue() as JSONObject
+
+                    // val jsonArray = jsonObject.getJSONObject(i)
+                    // results
+                    val results = jsonObject.getJSONArray("results")
+
+                    for (i in 0 until results.length()) {
+
+                        val iter = results.getJSONObject(i)
+
+                        Log.i("results: ", results.toString())
+
+                        val nameObj = iter.getJSONObject("tags_name")
+                        val name = nameObj.getString("raw")
+                        Log.i("name: ", name.toString())
+                        val sacObj = iter.getJSONObject("tags_sac_scale")
+                        val sacType = sacObj.getString("raw")
+                        Log.i("Sac Scale: ", sacType.toString())
+                        val xCoordObj = iter.getJSONObject("geometry_coordinates_0_1")
+                        val xCoord = xCoordObj.getString("raw")
+                        val yCoordObj = iter.getJSONObject("geometry_coordinates_1_0")
+                        val yCoord = yCoordObj.getString("raw")
+                        val idObj = iter.getJSONObject("id")
+                        val trailID = idObj.getString("raw")
+
+                        val model = SearchModel(
+                            trailID,
+                            name,
+                            sacType,
+                            xCoord,
+                            yCoord,
+                        )
+
+                        tmpArr.add(model)
+                        adapter = RecyclerAdapter(tmpArr)
+                        adapter!!.notifyDataSetChanged()
+                        Log.d("model: ", model.toString())
+                        Log.d("tmp arr: ", tmpArr.toString())
+
+                    }
+
+                    binding.recyclerView.adapter = adapter
+                }
+            } else {
+                Log.e("HTTPURLCONNECTION_ERROR", responseCode.toString())
+            }
+        }
+        return tmpArr
+    }
+
+/*    fun parseJSON(): java.util.ArrayList<DataModel> {
         var tmpArr: java.util.ArrayList<DataModel> = java.util.ArrayList()
         GlobalScope.launch(Dispatchers.IO) {
             val url =
@@ -115,6 +251,6 @@ class NewActivityFragment : Fragment() {
             }
         }
         return tmpArr
-    }
+    }*/
 
 }
